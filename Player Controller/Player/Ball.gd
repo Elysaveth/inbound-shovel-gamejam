67bbf6e1919a9b7c045b1b558e360b369aeb7160
Lizzy <martinezdelgadoelisabeth@gmail.com>
2025-07-16 11:47:50 +0200

    added camera, need to fix movement to camera

--- Player Controller/Player/Ball.gd
+++ Player Controller/Player/Ball.gd
@@ -1,12 +1,11 @@
 extends RigidBody3D
 
 @export_group("Movement")
-@export var rolling_speed := 40
+@export var rolling_speed := 5
 var is_running := false
 
 @export_group("Camera")
-@export var look_sensitivity : float = 0.025
-var camera_look_input : Vector2
+@export var look_sensitivity : float = 0.5
 
 @onready var camera : Camera3D = $CameraRig/Camera
 
@@ -17,22 +16,38 @@ func _ready():
 
 func _physics_process(delta):
 	var rolling_force = rolling_speed
+	
+	# Camera follow
 	$CameraRig.global_transform.origin = lerp(
-	$CameraRig.global_transform.origin, 
-	global_transform.origin, 0.1
+		$CameraRig.global_transform.origin, 
+		global_transform.origin,
+		0.1
 	)
+	$CameraRig.global_transform.basis.z = lerp(
+		$CameraRig.global_transform.basis.z,
+		Vector3(-linear_velocity.x, 0, -linear_velocity.z).normalized(),
+		look_sensitivity
+	)
+	$CameraRig.global_transform.basis.x = lerp(
+		$CameraRig.global_transform.basis.x,
+		Vector3(linear_velocity.z, 0, -linear_velocity.x).normalized(),
+		look_sensitivity
+	)
+	
 	$FloorCheck.global_transform.origin = global_transform.origin
+	
 	if is_running:
 		rolling_force = rolling_force * 1.5
 		is_running = false
+		
 	if Input.is_action_pressed("move_forward"):
-		angular_velocity.x -= rolling_force*delta
+		linear_velocity *= 1 + (rolling_force * delta)
 	elif Input.is_action_pressed("move_back"):
-		angular_velocity.x += rolling_force*delta
+		linear_velocity /= 1 + (rolling_force * delta)
 	if Input.is_action_pressed("move_left"):
-		angular_velocity.z += rolling_force*delta
+		linear_velocity -= rolling_force * delta
 	elif Input.is_action_pressed("move_right"):
-		angular_velocity.z -= rolling_force*delta
+		linear_velocity += rolling_force * delta
 		
 		# Mouse
 	if Input.is_action_just_pressed("ui_cancel"):
@@ -46,8 +61,3 @@ func _physics_process(delta):
 		apply_impulse(Vector3(), Vector3.UP*1000)
 		
 	is_running = Input.is_action_pressed("sprint")
-
-	camera.rotate_y((linear_velocity.angle_to(Vector3(camera.rotation.z, linear_velocity.y, camera.rotation.z))) * look_sensitivity)
-	camera.rotation.y = clamp(camera.rotation.y, -1.5, 1.5)
-
-	camera_look_input = Vector2.ZERO
